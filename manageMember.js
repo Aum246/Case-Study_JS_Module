a373a179b71b02d6ef6a1166b0ae641ca9b4b190
Hoai Thu <hthu246@gmail.com>
2023-03-26 21:56:24 +0700

    Finish JS case study

--- manageMember.js
+++ manageMember.js
@@ -1,7 +1,8 @@
-let memberList = ["Autumn", "Jason", "Winny", "Kayden", "Emma", "Dai"];
-let card = ["Master Card", "Visa Card", "American Express", "Napass", "Napass", "Master Card"];
+let memberList = ["Autumn", "Dai", "Hien", "Henry", "Emma", "Jason"];
+let card = ["Master Card", "Visa Card", "American Express", "Napas", "Napas", "Master Card"];
 let images = ["./image/1.png", "./image/2.png", "./image/3.png", "./image/4.png", "./image/5.png", "./image/6.png"];
-
+let imgTemp;
+let imgTemp_1;
 //Shown all member
 function showAllMember() {
     let content = ""
@@ -10,7 +11,7 @@ function showAllMember() {
             "<td class ='inputMember'>" + memberList[i] + "</td>" +
             "<td class ='imageMember'><img src='" + images[i] + "'></td>" +
             "<td class ='cardMember'>" + card[i] + "</td>" +
-            "<td><button onclick = 'delMember(" + i + ")'>Delete</button></td>" +
+            "<td><button onclick ='editMember(" + i + ")'>Edit</button> <button onclick = 'delMember(" + i + ")'>Delete</button></td>" +
             "</tr>"
     }
     document.getElementById("member").innerHTML = content;
@@ -18,14 +19,29 @@ function showAllMember() {
 //All members are shown including newest one.
 showAllMember();
 
-// Delete member
-// Check array index 
-function delMember(index) {
-    let check = confirm("Are you sure to delete this?");
-    if (check == true) {
-        memberList.splice(index, 1);
-        card.splice(index, 1);
-        images.splice(index, 1);
+//Upload photo
+function chooseFileCreate(fileInput) {
+    if (fileInput.files && fileInput.files[0]) {
+        let reader = new FileReader();
+
+        reader.onload = function (e) {
+            document.getElementById('image').setAttribute('src', e.target.result);
+        }
+        reader.readAsDataURL(fileInput.files[0]);
+    }
+}
+
+// Create a new member
+function createNewMember() {
+    let newMember = document.getElementById("newMember").value;
+    let cardMember = document.getElementById("cardMember").value;
+    let imgMember = document.getElementById('image').getAttribute('src');
+    if (newMember == '') {
+        document.getElementById("errorName").style.display = "block";
+    } else if (newMember !== '') {
+        card.push(cardMember);
+        memberList.push(newMember);
+        images.push(imgMember);
         showAllMember();
     }
 }
@@ -51,24 +67,54 @@ function searchMember() {
             "<td>" + memberSearch[i] + "</td>" +
             "<td><img src='" + imagesSearch[i] + "'></td>" +
             "<td>" + cardSearch[i] + "</td>" +
-            "<td><button onclick = 'delMember(" + i + ")'>Delete</button></td>" +
+            "<td><button onclick ='editMember(" + i + ")'>Edit</button> <button onclick = 'delMember(" + i + ")'>Delete</button></td>" +
             "</tr>"
     }
     document.getElementById("member").innerHTML = content;
 }
 
-//Upload photo
-function chooseFileCreate(fileInput) {
+// Edit member: name && photo - can not edit card member
+// Check array index
+let editCheck = true; // to edit on the same row
+function editMember(index) {
+    let changeName = document.getElementsByClassName('inputMember')[index].innerHTML;
+    if (changeName == memberList[index] && editCheck == true) {
+        document.getElementsByClassName('inputMember')[index].innerHTML = '<input type="text"  id="changeNameMember" value="' + changeName + '">';
+        document.getElementsByClassName('imageMember')[index].innerHTML = '<input type="file"  id="changeImageMember" onChange="chooseFileChange(this)" >';
+        editCheck = false;
+    }
+    if (changeName != memberList[index] && editCheck == false) {
+        memberList[index] = document.getElementById('changeNameMember').value;
+        document.getElementsByClassName('inputMember')[index].innerHTML = memberList[index];
+        images[index] = document.getElementById('changeImageMember').value;
+        document.getElementsByClassName('imageMember')[index].innerHTML = images[index];
+        images[index] = imgTemp;
+        editCheck = true;
+        showAllMember();
+    }
+}
+
+//Upload changed photo
+function chooseFileChange(fileInput) {
     if (fileInput.files && fileInput.files[0]) {
         let reader = new FileReader();
 
         reader.onload = function (e) {
-            document.getElementById('image').setAttribute('src', e.target.result);
+            imgTemp = e.target.result;
         }
         reader.readAsDataURL(fileInput.files[0]);
+        console.log(fileInput.files[0])
     }
 }
 
-// Create a new member
-function createNewMember() {
+// Delete member
+// Check array index 
+function delMember(index) {
+    let check = confirm("Are you sure to delete this?");
+    if (check == true) {
+        memberList.splice(index, 1);
+        card.splice(index, 1);
+        images.splice(index, 1);
+        showAllMember();
+    }
 }
